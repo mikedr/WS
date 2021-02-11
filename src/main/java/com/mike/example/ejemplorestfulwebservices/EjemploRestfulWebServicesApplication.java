b40dfa8d2b69684f0dad871bc9b8bbcfaf04d6df
mikedr <derosamiguel@gmail.com>
2021-02-11 01:26:54 -0300

    001-Aplicacion base

---
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/EjemploRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mike.example.ejemplorestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/example/ejemplorestfulwebservices/EjemploRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.example.ejemplorestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
