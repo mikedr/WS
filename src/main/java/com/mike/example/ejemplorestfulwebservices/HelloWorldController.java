e431c8d56cada73d33833703c5d6ce35b0c2d520
mikedr <derosamiguel@gmail.com>
2021-02-12 00:31:24 -0300

    003-Mejora del Hello world para retornar un bean

--- src/main/java/com/mike/example/ejemplorestfulwebservices/HelloWorldController.java
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.mike.example.ejemplorestfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path = "/helloWorld")
 	public String helloWorld() {
 		return "Hello world";
+	}
+	
+	@GetMapping(path = "/helloWorldBean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello world");
 	}
 }
