142b50762c1ec7ba9e71f2d8e7d37c417bf7ab68
mikedr <derosamiguel@gmail.com>
2021-02-13 16:52:22 -0300

    006-Implemento metodos GET para el recurso User

--- src/main/java/com/mike/example/ejemplorestfulwebservices/user/UserDAOService.java
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/user/UserDAOService.java
@@ -1,5 +1,42 @@
 package com.mike.example.ejemplorestfulwebservices.user;
 
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserDAOService {
 
+	private static List<User> users = new ArrayList<User>();
+	
+	private static int userCount = 3;
+	
+	static {
+		users.add(new User(1,"Leo", new Date()));
+		users.add(new User(2,"Enzo", new Date()));
+		users.add(new User(3,"Marcelo", new Date()));
+	}
+	
+	public List<User> findAll() {
+		return users;
+	}
+	
+	public User SaveUser(User user) {
+		if(user.getId()==null) {
+			user.setId(++userCount);
+		}
+		users.add(user);
+		return user;
+	}
+	
+	public User findOne(int id) {
+		for (User user : users) {
+			if(id == user.getId()) {
+				return user;
+			}
+		}
+		return null;
+	}
 }
---
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/user/UserResource.java
@@ -0,0 +1,25 @@
+package com.mike.example.ejemplorestfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	@Autowired 
+	UserDAOService userDAOService;
+	
+	@GetMapping(path = "/users/{id}")
+	public User getUser(@PathVariable int id) {
+		return userDAOService.findOne(id);
+	}
+	
+	@GetMapping(path = "/users")
+	public List<User> getAll() {
+		return userDAOService.findAll();
+	}
+}
