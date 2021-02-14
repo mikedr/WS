990ea9ee1f9ec11072ab4b0e94116f5476f2d7b2
mikedr <derosamiguel@gmail.com>
2021-02-14 01:55:51 -0300

    008-Mejoro POST. Status correcto y location

--- src/main/java/com/mike/example/ejemplorestfulwebservices/user/UserResource.java
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.mike.example.ejemplorestfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,7 +29,14 @@ public class UserResource {
 	}
 	
 	@PostMapping(path = "/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = userDAOService.saveUser(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		System.out.println(location);
+		
+		return ResponseEntity.created(location).build();
 	}
 }
