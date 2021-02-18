25234c94a2f36601a7f56a20cbadb4fc2ce53788
mikedr <derosamiguel@gmail.com>
2021-02-18 01:05:44 -0300

    013-Implemento validaciones sobre User

--- src/main/java/com/mike/example/ejemplorestfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.mike.example.ejemplorestfulwebservices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +22,10 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), "Validation failed");
+		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
 }
--- src/main/java/com/mike/example/ejemplorestfulwebservices/user/User.java
+++ src/main/java/com/mike/example/ejemplorestfulwebservices/user/User.java
@@ -2,12 +2,17 @@ package com.mike.example.ejemplorestfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
 	
+	@Size(min=2, message="La longitud minima del nombre es 2 caracteres")
 	private String name;
 	
+	@Past
 	private Date birth;
 
 	protected User() {
